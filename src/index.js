53fa6831cdc571d96aad123a3dfbdcfb36eaee4a
aleksei.bulgak <a.bulgak@intetics.com>
2018-09-25 21:48:43 +0300

    feature: bassic implementation. Need to fix issue with big numbers and time of calculation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function count(s, pairs) {
   // your implementation
+  var N = 1;
+  pairs.forEach(pair => {
+    N *= pair[0] ** pair[1];
+  });
+
+  if(!Number.isFinite(N)){
+    console.log("Problemo")
+    return 0;
+  }
+
+  let count = 0;
+  for (let k = 0; k <= N; k++) {
+    let countForK = 0;
+    s.split('').forEach((element, index) => {
+      let sum = k + index;
+      if (sum > 0) {
+        let result = moreThanOneGCF(sum);
+        if (!result && element == 1) {
+          countForK++;
+        } else if (result && element == 0) {
+          countForK++;
+        }
+      }
+    });
+    if(countForK === s.length){
+      count++;
+    }
+  }
+
+  function moreThanOneGCF(number) {
+    let isMoreThan1GCF = false;
+    if (number == 0 || number == 1) {
+      return isMoreThan1GCF;
+    }
+    pairs.forEach(pair => {
+      isMoreThan1GCF = isMoreThan1GCF || number % pair[0] === 0;
+    })
+    return isMoreThan1GCF;
+  }
+
+  console.log(count)
+  return count % 1000000007;
 }
+
+module.exports('1011', [[3, 1000000000]]);
